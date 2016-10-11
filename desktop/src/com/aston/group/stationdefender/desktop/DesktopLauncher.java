a28141f6259809822c60741988a46eec9561b098
Jonathon Fitch <lightman1@virginmedia.com>
2016-10-11 03:43:44 +0100

    DesktopLauncher: Change title to "Station Defender"

--- desktop/src/com/aston/group/stationdefender/desktop/DesktopLauncher.java
+++ desktop/src/com/aston/group/stationdefender/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.aston.group.stationdefender.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Station Defender";
 		new LwjglApplication(new Main(), config);
 	}
 }
