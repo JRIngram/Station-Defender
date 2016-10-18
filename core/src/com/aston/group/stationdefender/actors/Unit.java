3e82eaef9a3b47b38152d7f07b4a817e41856efd
Jonathon Fitch <lightman1@virginmedia.com>
2016-10-18 10:15:41 +0100

    Unit: Add range variable

--- core/src/com/aston/group/stationdefender/actors/Unit.java
+++ core/src/com/aston/group/stationdefender/actors/Unit.java
@@ -2,7 +2,7 @@ package com.aston.group.stationdefender.actors;
 
 public class Unit implements Actor {
     private String name;
-    private double speed, damage, rateOfFire, health;
+    private double speed, damage, rateOfFire, health, range;
 
     public Unit() {
     }
