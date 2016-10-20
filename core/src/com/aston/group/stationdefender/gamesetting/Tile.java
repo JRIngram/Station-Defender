09bbe33545ffec73da4a022f96236cf116214adf
Jonathon Fitch <lightman1@virginmedia.com>
2016-10-20 22:01:53 +0100

    Tile: Remove redundant code, Unit is now abstract

--- core/src/com/aston/group/stationdefender/gamesetting/Tile.java
+++ core/src/com/aston/group/stationdefender/gamesetting/Tile.java
@@ -1,10 +1,6 @@
 package com.aston.group.stationdefender.gamesetting;
 
-import com.aston.group.stationdefender.actors.Unit;
-
 public class Tile {
-    private Unit unit = new Unit();
-
     public Tile() {
     }
 }
