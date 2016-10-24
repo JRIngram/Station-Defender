a09deeccc09aec7b177a3feaf3e42d2fb7099094
Jringram <jr-ingram@hotmail.co.uk>
2016-10-24 19:53:25 +0100

    Update Javadoc for Unit class.

--- core/src/com/aston/group/stationdefender/actors/Unit.java
+++ core/src/com/aston/group/stationdefender/actors/Unit.java
@@ -15,12 +15,81 @@ public abstract class Unit implements Actor {
     protected double health;
     protected double range;
 
-    public Unit(){
+    public Unit(String name, double speed, double damage, double rateOfFire, double health, double range){
+    	this.name = name;
+    	this.speed = speed;
+    	this.damage = damage;
+    	this.rateOfFire = rateOfFire;
+    	this.health = health;
+    	this.range = range;
     }
-
+    
+    /**
+     * Abstract method for each Unit, the main method which determines how the unit acts.
+     */
     @Override
     public abstract void act();
-
+    
+    /**
+     * Abstract method for each unit, destroys the unit.
+     */
     @Override
     public abstract void destroy();
+    
+    /**
+     * 
+     * @return Speed of the unit.
+     */
+    public double getSpeed(){
+    	return speed;
+    }
+    
+    /**
+     * 
+     * @return The damage that the unit inflicts.
+     */
+    public double getDamage(){
+    	return damage;
+    }
+    
+    /**
+     * 
+     * @return The rate of fire of the unit.
+     */
+    public double getRateOfFire(){
+    	return rateOfFire;
+    }
+    
+    /**
+     * 
+     * @return The current health of the unit.
+     */
+    public double getHealth(){
+    	return health;
+    }
+    
+    /**
+     * 
+     * @return The range that the unit can fire.
+     */
+    public double getRange(){
+    	return range;
+    }
+    
+    /**
+     * 
+     * @param damage Causes the Unit's health to deplete.
+     */
+    public void takeDamage(double damage){
+    	health = health - damage;
+    }
+    
+    /**
+     * Checks if the Unit is adjacent to another entity.
+     * @param entity An Actor.
+     * @return Boolean which says if the Unit is adjacent to another entity.
+     */
+    public boolean isAdjacent(Actor entity){
+    	return false;
+    }
 }
