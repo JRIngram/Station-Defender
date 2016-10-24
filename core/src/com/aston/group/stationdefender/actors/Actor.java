a332c781db89077f56a3a0e63656a198ac324972
Jonathon Fitch <lightman1@virginmedia.com>
2016-10-24 14:54:16 +0100

    Actor: Fix typo

--- core/src/com/aston/group/stationdefender/actors/Actor.java
+++ core/src/com/aston/group/stationdefender/actors/Actor.java
@@ -3,7 +3,7 @@ package com.aston.group.stationdefender.actors;
 /**
  * Interface implemented by the Unit and Tower classes.
  * Used for entities within the game that can act and be destroyed.
- * @author Jamie Ingram *
+ * @author Jamie Ingram
  */
 public interface Actor {
 	public void act();
