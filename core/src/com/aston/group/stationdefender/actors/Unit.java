d09bdbd5c5d2a724da93d2013b0d119aab572362
J. Ingram <JRIngram@users.noreply.github.com>
2016-11-03 12:04:04 +0000

    Added Accessor and Mutator methods for dimensions and coordinates

--- core/src/com/aston/group/stationdefender/actors/Unit.java
+++ core/src/com/aston/group/stationdefender/actors/Unit.java
@@ -226,4 +226,33 @@ public abstract class Unit implements Actor {
     	}
     	return (hit * damage);
     }
+	
+    public void setHeightAndWidth(int height, int width){
+    	this.height = height;
+	this.width = width;
+    }
+
+    public void setX(int x){
+     	this.x = x;
+    }
+
+    public void setY(int y){
+    	this.y = y;
+    }
+	
+    public int getHeight(){
+    	return height;
+    }
+	
+    public int getWidth(){
+    	return width;
+    }
+	
+    public int getX(){
+    	return x;
+    }
+
+    public int getY(){
+    	return y;
+    }
 }
