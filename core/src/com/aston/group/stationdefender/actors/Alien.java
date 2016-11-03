1146ad6e3982e6dfb0ede69a89e3e1499fb7dc56
J. Ingram <JRIngram@users.noreply.github.com>
2016-11-03 12:30:12 +0000

    Allow setting of dimensions and coordinates in Alien Constructor

--- core/src/com/aston/group/stationdefender/actors/Alien.java
+++ core/src/com/aston/group/stationdefender/actors/Alien.java
@@ -8,7 +8,7 @@ package com.aston.group.stationdefender.actors;
 public abstract class Alien extends Unit{
 	
 	public Alien(String name,  double speed, double damage, double rateOfFire, double health, double range){
-		super(name, speed, damage, rateOfFire, health, range);
+		super(name, speed, damage, rateOfFire, health, range, x, y, height, width);
 	}
 
     @Override
