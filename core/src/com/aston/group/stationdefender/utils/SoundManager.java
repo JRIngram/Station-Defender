d58d46d9d76c5f2889bde07ab8015b2ecc60be72
Jonathon Fitch <lightman1@virginmedia.com>
2016-11-15 05:11:04 +0000

    SoundManager: Add javadoc comments
    Signed-off-by: Jonathon Fitch <lightman1@virginmedia.com>

--- core/src/com/aston/group/stationdefender/utils/SoundManager.java
+++ core/src/com/aston/group/stationdefender/utils/SoundManager.java
@@ -4,19 +4,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
 public class SoundManager {
-
     public static final int EXPLOSION_SOUND_ID = 1;
+    private static SoundManager instance;
 
-    public static SoundManager instance;
-
+    /**
+     * Creates an instance of the SoundManager
+     */
     public SoundManager() {
         instance = this;
     }
 
+    /**
+     * Returns an instance of the SoundManager
+     *
+     * @return Returns an instance of the SoundManager
+     */
     public static SoundManager getInstance() {
         return instance;
     }
 
+    /**
+     * Play the explosion sound
+     */
     public void playExplosion() {
         Music music = Gdx.audio.newMusic(Gdx.files.internal("sounds/Explosion.mp3"));
         music.setLooping(true);
