2b6bc12c60075aa5f42ed7beaa4394f9ea125377
Jonathon Fitch <lightman1@virginmedia.com>
2016-11-20 11:27:29 +0000

    Cleanup Board
    Signed-off-by: Jonathon Fitch <lightman1@virginmedia.com>

--- core/src/com/aston/group/stationdefender/gamesetting/Board.java
+++ core/src/com/aston/group/stationdefender/gamesetting/Board.java
@@ -4,7 +4,6 @@ import com.aston.group.stationdefender.actors.Actor;
 import com.aston.group.stationdefender.actors.Tower;
 import com.aston.group.stationdefender.actors.Unit;
 import com.aston.group.stationdefender.config.Constants;
-import com.aston.group.stationdefender.utils.ProjectileFactory;
 import com.badlogic.gdx.utils.Array;
 
 /**
@@ -14,23 +13,10 @@ import com.badlogic.gdx.utils.Array;
  * @author Twba Alshaghdari
  */
 public class Board {
-    private static final int numberOfLanes = 4;
-    private static final int numberOfTiles = 4;
     private final Array<Lane> lanes = new Array<>();
     private final Tower tower;
-    private ProjectileFactory projectileFactory;
     private boolean hasWon;
     private boolean hasLost;
-
-    /**
-     * Construct a new Board with a default of 4 Lanes
-     *
-     * @param player The current Player
-     * @param tower  The current Tower on the Board
-     */
-    public Board(Player player, Tower tower) {
-        this(player, tower, numberOfLanes, numberOfTiles);
-    }
 
     /**
      * Construct a new Board with a given number of Lanes each lane will have
@@ -38,14 +24,12 @@ public class Board {
      *
      * @param player        The current Player
      * @param tower         The current Tower on the Board
-     * @param numberOfLanes The number of Lanes for the Board to have
-     * @param numberOfTiles The number of Lanes for the Board to have
      */
-    public Board(Player player, Tower tower, int numberOfLanes, int numberOfTiles) {
+    Board(Player player, Tower tower) {
         this.tower = tower;
         int laneY = 120;
 
-        for (int i = 0; i < numberOfLanes - 1; i++) {
+        for (int i = 0; i < Constants.LANE_AMOUNT; i++) {
             lanes.add(new Lane(player, tower, 100, laneY, Constants.TILE_AMOUNT));
             laneY += (Constants.TILE_HEIGHT + (Constants.TILE_HEIGHT / 2));
         }
@@ -131,27 +115,28 @@ public class Board {
     public void render(float delta) {
         for (Lane lane : lanes) {
             lane.render(delta);
-
             if (lane.isOverrun()) {
                 if (!tower.getExists())
                     hasLost = true;
             }
         }
-
         if (isAllLanesCleared()) {
             hasWon = true;
         }
     }
 
-    public boolean isAllLanesCleared() {
+    /**
+     * Returns whether all the lanes are clear or not
+     *
+     * @return true if all the lanes are cleared, false if any of the lanes are not cleared
+     */
+    private boolean isAllLanesCleared() {
         boolean cleared = true;
-
         for (int i = 0; i < lanes.size; i++) {
             if (!lanes.get(i).isCleared()) {
                 cleared = false;
             }
         }
-
         return cleared;
     }
 
@@ -164,19 +149,21 @@ public class Board {
         }
     }
 
-    public boolean isHasWon() {
+    /**
+     * Returns whether the Player has won the game or not
+     *
+     * @return true if the Player has won, false if the Player has not won
+     */
+    boolean isHasWon() {
         return hasWon;
     }
 
-    public void setHasWon(boolean hasWon) {
-        this.hasWon = hasWon;
-    }
-
-    public boolean isHasLost() {
+    /**
+     * Returns whether the Player has lost the game or not
+     *
+     * @return true if the Player has lost, false if the Player has not lost
+     */
+    boolean isHasLost() {
         return hasLost;
-    }
-
-    public void setHasLost(boolean hasLost) {
-        this.hasLost = hasLost;
     }
 }
