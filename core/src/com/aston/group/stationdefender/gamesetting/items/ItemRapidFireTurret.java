ae4fd70c030970628bf11b0c9e03cf155765877e
Jonathon Fitch <lightman1@virginmedia.com>
2017-03-30 22:48:55 +0100

    Increase RapidFireWeapon Item cost
    Signed-off-by: Jonathon Fitch <lightman1@virginmedia.com>

--- core/src/com/aston/group/stationdefender/gamesetting/items/ItemRapidFireTurret.java
+++ core/src/com/aston/group/stationdefender/gamesetting/items/ItemRapidFireTurret.java
@@ -14,7 +14,7 @@ public class ItemRapidFireTurret extends Item {
     public ItemRapidFireTurret() {
         super("Rapid Fire Turret");
         id = 3;
-        cost = 10;
+        cost = 15;
         texture = TextureManager.INSTANCE.loadTexture(13);
         placeable = true;
         sku = Items.TURRET;
