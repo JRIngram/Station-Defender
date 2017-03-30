d818943bd32533c7fdbadfaad002d2600c193319
Jonathon Fitch <lightman1@virginmedia.com>
2017-03-30 15:02:16 +0100

    Increment version to 2.0
    Signed-off-by: Jonathon Fitch <lightman1@virginmedia.com>

--- core/src/com/aston/group/stationdefender/config/Constants.java
+++ core/src/com/aston/group/stationdefender/config/Constants.java
@@ -63,7 +63,7 @@ public interface Constants {
     Color primaryColor = Color.valueOf("#37474F");
     Color primaryDarkColor = Color.valueOf("#263238");
 
-    float VERSION = 1.1f;
+    float VERSION = 2.0f;
 
     String prefs = "PREFS";
 }
